cbe9e2edc3d099f9f3df48a4e02c0af2240a013f
yayitswhit <104163475+yayitswhit@users.noreply.github.com>
2023-03-12 20:52:43 -0400

    firt commit

---
+++ Lab11/Lab11/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  Lab11
+//
+//  Created by Gilbert W. Sanborn Jr on 3/9/23.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string name;
+char character; //reads in character by character
+bool vowelOrNot; //is it a vowel or not?
+
+bool isVowel(char letter);
+int main() {
+    string name;
+    char character; //reads in character by character
+    bool vowOrNot; //is it a vowel or not?
+    int numOfVowels = 0;
+    cout << "Enter a sentence and then press enter:" << endl;
+    /*cin.get(character);
+    cout << character << endl;
+    cout << name << endl;*/
+
+    while(cin.get(character)) {
+        if(isVowel(character)) {
+            numOfVowels += 1;
+        }
+        cout << character << endl;
+    }
+    cout << numOfVowels << endl;
+    
+    vowOrNot = isVowel(character);
+    //cout << vowOrNot;
+    return 0;
+}
+
+bool isVowel(char letter)
+{
+    if(letter == 'A' || 'a' || 'E' || 'e' || 'I' || 'i' || 'O' || 'o' || 'U' || 'u') {
+        vowelOrNot = true;
+    }
+    else{
+        vowelOrNot = false;
+    }
+    return vowelOrNot;
+}
