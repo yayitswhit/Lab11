fc8c36942eae75f4b52c453f617b4263ff5d3474
yayitswhit <104163475+yayitswhit@users.noreply.github.com>
2023-03-13 20:19:38 -0400

    final draft

--- Lab11/Lab11/main.cpp
+++ Lab11/Lab11/main.cpp
@@ -1,4 +1,5 @@
-//
+//  This program will ask for user input in the form of a sentence. The sentenced will then
+//  be processed and the number of vowels in the string will be output.
 //  main.cpp
 //  Lab11
 //
@@ -18,23 +19,24 @@ bool isVowel(char letter);
 int main() {
     string name;
     char character; //reads in character by character
-    bool vowOrNot; //is it a vowel or not?
     int numOfVowels = 0;
     cout << "Enter a sentence and then press enter:" << endl;
-    /*cin.get(character);
-    cout << character << endl;
-    cout << name << endl;*/
-
-    while(cin.get(character)) {
+    while(character != '\n') {
+        cin.get(character);
         if(isVowel(character)) {
             numOfVowels += 1;
         }
-        cout << character << endl;
     }
-    cout << numOfVowels << endl;
     
-    vowOrNot = isVowel(character);
-    //cout << vowOrNot;
+      //  cout << character << endl;
+    
+    //make sure english grammar is correct by using the sigular form of "is" for one vowel, "are" for more than one
+    if(numOfVowels == 1) {
+        cout << "There is " << numOfVowels << " vowel." << endl;
+    }
+    else {
+        cout << "There are " << numOfVowels << " vowels." << endl;
+    }
     return 0;
 }
 
